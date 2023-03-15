2f4d18847d5bcb600e91c37df9158b725f877a84
Dimitris <dim-laskos@hotmail.com>
2023-03-15 15:33:01 +0100

    Updated medication controller APIs

--- services/medication-record/main.go
+++ services/medication-record/main.go
@@ -2,41 +2,36 @@ package main
 
 import (
 	"net/http"
-	"github.com/labstack/echo/v4"
+
 	"github.com/amundlrohne/dcs-medication-sharing/services/medication-record/routes"
+	"github.com/labstack/echo/v4"
 )
 
 func getMedication(c echo.Context) error {
-	id:=c.Param("id");
-	return c.String(http.StatusOK, "GET medication-record/" + id);
+	id := c.Param("id")
+	return c.String(http.StatusOK, "GET medication-record/"+id+"Heyyy Amund")
 }
 
 func postMedication(c echo.Context) error {
-	return c.String(http.StatusOK, "POST medication-record/");
+	return c.String(http.StatusOK, "POST medication-record/")
 }
 
 func putMedication(c echo.Context) error {
-	id:=c.Param("id");
-	return c.String(http.StatusOK, "PUT medication-record/" + id);
+	id := c.Param("id")
+	return c.String(http.StatusOK, "PUT medication-record/"+id)
 }
 
 func deleteMedication(c echo.Context) error {
-	id:=c.Param("id");
-	return c.String(http.StatusOK, "DELETE medication-record/" + id);
+	id := c.Param("id")
+	return c.String(http.StatusOK, "DELETE medication-record/"+id)
 }
 
-
-
 func main() {
+
 	e := echo.New()
 
-	//e.GET("/medication-record/:id", getMedication)
-	// e.POST("/medication-record/", postMedication)
-	// e.PUT("/medication-record/:id", putMedication)
-	// e.DELETE("/medication-record/:id", deleteMedication)
-
 	routes.MedicationRoute(e)
-	
+
 	e.Logger.Fatal(e.Start(":8080"))
-	
+
 }
