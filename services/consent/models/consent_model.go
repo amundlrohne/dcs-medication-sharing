01888aa99c9da98105e64c126ca538f0ec3786d2
stefanovstan <sss5@email.sc.edu>
2023-03-16 13:52:38 +0100

    add changes to consent api routes

--- services/consent/models/consent_model.go
+++ services/consent/models/consent_model.go
@@ -2,6 +2,7 @@ package models
 
 type Consent struct {
 	ToPublicKey   string `json:"topublickey,omitempty" validate:"required"`
-	FromPublicKey string `json:"frompublickey,omitempty" validate:"required"`
+	FromPublicKey string `json:"frompublickey,omitempty" validate:"required"` // use for GET method
 	ExpDate       string `json:"expdate,omitempty" validate:"required"`
+	DateCreated   string `json:"datecreated,omitempty" validate:"required"`
 }
