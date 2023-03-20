d8fdba22834af211d92ef61ebcee2eca444d8a70
Selin99 <48292786+Selin99@users.noreply.github.com>
2023-03-20 17:40:28 +0100

    deleteConsent,deleteTimeout

--- services/consent/routes/consent_route.go
+++ services/consent/routes/consent_route.go
@@ -9,6 +9,4 @@ func ConsentRoute(e *echo.Echo) {
 	e.POST("/createConsent", controllers.CreateConsent)
 	e.GET("/getConsent/:from_public_key", controllers.GetConsent)
 	e.GET("/getConsents", controllers.GetAllConsents)
-	e.DELETE("/deleteConsent/:from_public_key", controllers.DeleteConsent)
-	e.DELETE("/deleteTimeOut", controllers.TimeOut)
 }
