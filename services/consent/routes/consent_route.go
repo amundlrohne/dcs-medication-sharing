a50a910a4dfdda3ceab072d76a543395a92f0c95
amundlrohne <amundlrohne@gmail.com>
2023-03-22 10:53:59 +0100

    readiness checks and ingress config

--- services/consent/routes/consent_route.go
+++ services/consent/routes/consent_route.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/amundlrohne/dcs-medication-sharing/services/consent/controllers"
-	"github.com/amundlrohne/dcs-medication-sharing/services/consent/responses"
 	"github.com/labstack/echo/v4"
 )
 
@@ -12,10 +11,10 @@ func ConsentRoute(e *echo.Echo) {
 	e.POST("/createConsent", controllers.CreateConsent)
 	e.GET("/getConsent/:from_public_key", controllers.GetConsent)
 	e.GET("/getConsents", controllers.GetAllConsents)
-	e.GET("/isalive", IsAlive)
+	e.GET("/", IsAlive)
 }
 
 func IsAlive(c echo.Context) error {
 
-	return c.JSON(http.StatusOK, responses.ConsentResponse{Status: http.StatusOK, Message: "success", Data: &echo.Map{"data": "OK"}})
+	return c.String(http.StatusOK, "Alive")
 }
--- services/standardization/routes/medication_route.go
+++ services/standardization/routes/medication_route.go
@@ -1,10 +1,18 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/amundlrohne/dcs-medication-sharing/services/standardization/controllers"
 	"github.com/labstack/echo/v4"
 )
 
 func MedicationRoute(e *echo.Echo) {
 	e.GET("/standardization/:drugName", controllers.SearchDrug)
+	e.GET("/", IsAlive)
+}
+
+func IsAlive(c echo.Context) error {
+
+	return c.String(http.StatusOK, "Alive")
 }
