4655c3eca95aec8019b69ae54b618602bdb37c15
amundlrohne <amundlrohne@gmail.com>
2023-03-23 00:47:33 +0100

    fix logout url

--- services/react-frontend/src/components/Navbar.js
+++ services/react-frontend/src/components/Navbar.js
@@ -1,61 +1,69 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
-import '../css/Navbar.css'
+import "../css/Navbar.css";
 
 import { resolveURL } from "../util/resolveURL";
 
 const Navbar = () => {
+    const [logoutButton, showLogoutButton] = useState(false);
 
-    const [logoutButton, showLogoutButton] = useState(false)
-
-    useEffect(async() => {
-        let pathname = window.location.pathname
-        if (pathname !== '/login') {
+    useEffect(async () => {
+        let pathname = window.location.pathname;
+        if (pathname !== "/login") {
             let cookieData = await getCookie();
             console.log(cookieData);
 
             if (cookieData.message === "success") {
-                showLogoutButton(true)
+                showLogoutButton(true);
             }
         }
-    }, [])
+    }, []);
 
     const logoutUser = async () => {
-        let response = await deleteCookie()
-        
-        window.location = '/login'
-    }
+        let response = await deleteCookie();
+
+        window.location = "/login";
+    };
 
     const deleteCookie = async () => {
-        let url = `${resolveURL("healthcare-provider")}/healthcare-provider`;
+        let url = `${resolveURL("healthcare-provider")}/healthcare-provider/`;
         const response = await fetch(url, {
             method: "DELETE",
             mode: "cors",
             credentials: "include",
         });
-    
+
         return response.json();
     };
 
     const getCookie = async () => {
-        let url = `${resolveURL("healthcare-provider")}/healthcare-provider/current`;
+        let url = `${resolveURL(
+            "healthcare-provider"
+        )}/healthcare-provider/current`;
         const response = await fetch(url, {
             method: "GET",
             mode: "cors",
             credentials: "include",
         });
-    
+
         return response.json();
     };
 
     return (
-        <div className='navbar'>
+        <div className="navbar">
             <p>MedShare</p>
             {logoutButton && (
-                <button onClick={() => {logoutUser()}} className='logout-button'>Logout</button>
+                <button
+                    onClick={() => {
+                        logoutUser();
+                    }}
+                    className="logout-button"
+                >
+                    Logout
+                </button>
             )}
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
